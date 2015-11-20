4ff652179efd6fb86edca06470cc162579a8e46b
Eric Klein <eric.j.klein@chicagobooth.edu>
2015-11-20 16:37:34 -0600

    Included ability to edit username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:sign_up) << :avatar_url
 
+    devise_parameter_sanitizer.for(:account_update) << :username
     devise_parameter_sanitizer.for(:account_update) << :avatar_url
   end
 
